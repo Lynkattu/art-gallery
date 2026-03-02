e7065e0599dca8efcc8e44aca177028f94b59c55
Tuomas Nissinen <tuomas.nissinen@student.lab.fi>
2026-03-02 21:03:46 +0200

    readme updated, art page improvements

--- front-end/art-gallery/README.md
+++ front-end/art-gallery/README.md
@@ -1,1 +1,28 @@
-# Installation
+# Art Gallery
+Art gallery is under development, its not responsive and many features are not implemented yet.
+
+## Installation
+1. Download project from git.
+2. Open project in code editor and install node modules for front-end and back-end. 
+3. Install mysql database if you do not have it already. Create database, you will need database password, name and user on later steps.
+4. Create tables for your database. Table structure can be found from tables file, navigate to back-end -> db -> tables.
+5. Create .env file under back-end folder. the File must contain next data: 
+<br>
+DB_PASSWORD = your database password
+<br>
+DB = your database
+<br>
+DB_PORT = 3306
+<br>
+DB_HOST = "127.0.0.1"
+<br>
+DB_USER = database user
+<br>
+HOST = "localhost"
+<br>
+PORT = 5000
+<br>
+JWT_SECRET = your jwt secret (can be anything)
+<br>
+NODE_ENV=development
+6. Frontend starts like any react app with *npm run dev* command. Backend start running also with *npm run dev* command. Note you must run commands under front-end folder or back-end folder.
--- front-end/art-gallery/src/art.tsx
+++ front-end/art-gallery/src/art.tsx
@@ -8,17 +8,39 @@ function Art() {
     const location = useLocation();
     const { art } = location.state || {};
 
-    const artState: JSX.Element = (            
-        <div>
-            <h4>{art.title}</h4>
-            <p>{art.description}</p>
-            <img src={art.imageUrl} alt={art.title} />
-        </div>
+    const artPage: JSX.Element = (
+        <div className="art-page">
+            <div className="art">
+            {/*left*/}
+            <div className="content">
+                <h4>{art.title}</h4>
+                <img src={art.imageUrl} alt={art.title} />
+                <p>{art.description}</p>
+
+            </div>
+            {/*right divided horizantally to half*/}
+            <div className="recommendations" >
+                <div className="recommended-art">
+                    <h5>Recommended Art</h5>
+                </div>
+                <div className="artist-art">
+                    <h5>More from this Artist</h5>
+                </div>
+            </div>
+            </div>
+            
+            {/*bottom*/}
+            <div className="comments">
+                <h5>Comments</h5>
+            </div>
+
+        </div>          
+
     );
 
-    return <div className="art">
+    return <div >
         <Topbar />
-        {art && Object.keys(art).length > 0 ? artState : null}
+        {art && Object.keys(art).length > 0 ? artPage : null}
     </div>;
 }
 
